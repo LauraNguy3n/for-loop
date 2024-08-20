93eb72e96a9ef41dae4c5a2f5eeb426fb1cca76e
laura <you@example.com>
2024-08-20 12:40:38 -0500

    hallo :D!

---
+++ For_Loop/src/forloop/ForLoop.java
@@ -0,0 +1,75 @@
+package forloop;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		
+		
+		/*for loop:
+			1. initialization
+			2. condition
+			3. increment/decrement
+			4. statement
+			
+			for(initialization; condition; increment/decrement) {
+				statement;
+			}
+	
+	
+	
+	
+	    while loop:
+	    	1.initilization
+	    	2. condition
+	    	3. statement
+	    	4. increment/decrement
+	    	
+	    	
+	    	initilization;
+	    
+	        while(condition) {
+	        	statement;
+	        	increment/decrement;
+	        }
+	        
+	        int i=1;
+	        while(i<=10) {
+	        	System.out.println(i);
+	        	i++;
+		
+			
+			for(int x=1; x<=10; x++) {
+				System.out.println(x);
+				
+				
+	        }
+	        
+		int x=4;
+		if(x%2==0) {
+			System.out.println(x + " is an even number.");
+		}
+		
+		else {
+			System.out.println(x + " is not an even number.");
+		}
+		
+	
+		int hello =1;
+		int world = 10;
+		for(int h = hello; h <= world; h++) {
+			if (h%2 !=0) {
+				System.out.println(h);
+			}
+			
+		}
+*/
+		
+		for(int y=1; y<=20;y++) {
+		System.out.println("Laura Nguyen");
+
+	}
+
+		
+		
+	}
+}
